5a155bf74e83a752867933150faa60af8e69731b
MatveyRubets <rmatvey11@gmail.com>
2022-04-20 10:33:21 +0200

    goit-js-hw-10

---
+++ src/getRefs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+	return {
+		inputSearchRef: document.querySelector("#search-box"),
+		countriesListRef: document.querySelector(".country-list"),
+		countryInfoRef: document.querySelector(".country-info"),
+	};
+}
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import "./css/styles.css";
+
+import debounce from "lodash.debounce";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+import API from "./fetchCountries";
+import getRefs from "./getRefs";
+import MarkupService from "./markup-functions";
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = getRefs();
+
+refs.inputSearchRef.addEventListener(
+	"input",
+	debounce(onInputSearch, DEBOUNCE_DELAY)
+);
+
+function onInputSearch(e) {
+	const query = e.target.value.trim();
+
+	if (!query) {
+		MarkupService.deleteCountry();
+		MarkupService.deleteCountries();
+		return;
+	}
+	e.target.value = query;
+	API.fetchCountryByName(query).then(renderMarkup).catch(onFetchError);
+}
+
+function renderMarkup(countries) {
+	if (countries.length >= 10) {
+		MarkupService.deleteCountry();
+		MarkupService.deleteCountries();
+		Notify.info("Too many matches found. Please enter a more specific name.");
+		return;
+	}
+
+	if (countries.length >= 2 && countries.length < 10) {
+		MarkupService.deleteCountry();
+		MarkupService.getCountries(countries);
+		return;
+	}
+
+	MarkupService.deleteCountries();
+	MarkupService.getCountryByName(countries);
+}
+
+function onFetchError() {
+	Notify.failure("Oops, there is no country with that name");
+	MarkupService.deleteCountry();
+	MarkupService.deleteCountries();
+}
